2d6498e451f423cecdc746a2bf92a736232e5381
huanghui0602 <2637871558@qq.com>
2019-12-30 16:29:39 +0800

    chao

---
+++ student_sys/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Administrator\PycharmSpace\student_house\student_sys\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
